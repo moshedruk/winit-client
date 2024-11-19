2ebba97cf3c940f858be7eeb50ee9eecdff3fff0
moshedruk <dmd6708713@gmail.com>
2024-11-19 17:42:44 +0200

    add btn for login

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,13 +1,16 @@
 import React, { useState } from 'react'
+import { useAppDispatch } from '../redux/store';
+import { fetchLogin } from '../redux/slices/user';
 
 export default function Login() {
+    const dispatch = useAppDispatch();
     const [username,setUsername] = useState("")
     const [password,setPassword] = useState("")
   return (
     <div className='login-user'>
       <input className='inp-login-user' type='text' placeholder='username' value={username} onChange={(e)=>setUsername(e.target.value)}/>
       <input className='inp-login-user' type='password' placeholder='password' value={password} onChange={(e)=>setPassword(e.target.value)}/>
-      <button className='btn-user-login' onClick={()=>console.log(username)}>Login</button>      
+      <button className='btn-user-login'  onClick={() => dispatch(fetchLogin({ username, password }))}>Login</button>      
     </div>
   )
 }
